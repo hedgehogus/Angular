5a73811b5fa858fe2aa8ef6857f9a9314962d549
hedgehogus <research162@gmail.com>
2017-10-06 05:28:04 +0300

    dose not works

--- accelerating-through-angular/my-app/src/main.ts
+++ accelerating-through-angular/my-app/src/main.ts
@@ -1,12 +1,36 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {NgModule, Component} from '@angular/core';
+//BrowserModule - module needed for running Angular websites
+import {BrowserModule} from '@angular/platform-browser'
+//this will allow us to bootstrap, or launch the app
+import {platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// decorator
+//@Component used to apply our component decorator to our class;
+@Component({
+    selector: 'my-app', // the css selector for the HTML element where we want the component to load
+    template: `<h1>{{title}}</h1>
+    <h2>{{carPart.name}}</h2>
+    <p>{{carPart.description}}</p>
+    <p>{{carPart.inStock}} in Stock</p>` //the content we want to load inside our selector
+})
 
-if (environment.production) {
-  enableProdMode();
+// class
+class AppComponent {
+    title = 'Ultra Racing';
+    carPart = {
+        "id":1,
+        "name":"super Tires",
+        "description":"these tires are the very best",
+        "inStock":5
+    }
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+@NgModule({
+    declaration: [AppComponent],
+    imports: [BrowserModule], // loads required dependencies to launch our app in the browser
+    bootstrap: [AppComponent] //indicates our root component
+})
+class AppModule {}
+
+platformBrowserDynamic()
+    .bootstrapModule(AppModule); // render application using AppModule
