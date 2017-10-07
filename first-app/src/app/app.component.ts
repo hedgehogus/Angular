ba5431b003a835fbc8ce77ff3724cf78f8598bf5
hedgehogus <research162@gmail.com>
2017-10-07 06:17:24 +0300

    first app

---
+++ first-app/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{carPart.name}}</h2>
+    <p>{{carPart.description}}</p>
+    <p>{{carPart.inStock}} in Stock</p>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ultra Racing';
+  carPart = {
+        "id":1,
+        "name":"super Tires",
+        "description":"these tires are the very best",
+        "inStock":5
+    }
+}
