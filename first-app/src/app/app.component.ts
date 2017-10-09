bbddfeb7c80cc6458f369b4cec4f0fd75dc7b141
hedgehogus <research162@gmail.com>
2017-10-09 23:19:46 +0300

    ng directives

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,21 +1,42 @@
 import { Component } from '@angular/core';
 
+  //  ngFor - is a structural directive
+  // carPar - is a local variable
+  // carParts is the array to loop through
+
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>{{title}}</h1>
-    <h2>{{carPart.name}}</h2>
-    <p>{{carPart.description}}</p>
-    <p>{{carPart.inStock}} in Stock</p>`,
+  template: `  
+    <h1>{{title}}</h1>    
+    <ul>      
+      <li *ngFor="let carPart of carParts">
+        <h2>{{carPart.name}}</h2>
+        <p>{{carPart.description}}</p>
+        <p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p>
+        <p *ngIf="carPart.inStock === 0">Out of Stock</p>
+      </li>
+    </ul>`,
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   title = 'Ultra Racing';
-  carPart = {
+  carParts = [{
         "id":1,
         "name":"super Tires",
         "description":"these tires are the very best",
         "inStock":5
-    }
+    },
+    {
+        "id":2,
+        "name":"Reinforced Shoks",
+        "description":"these tires are the very best",
+        "inStock":4
+    },
+    {
+        "id":3,
+        "name":"super Hedgehogs",
+        "description":"these Hedgehogs are hoglets",
+        "inStock":0
+    }]
 }
