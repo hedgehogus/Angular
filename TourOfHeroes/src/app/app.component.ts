16c630403568b50bc5d5483e9a65256943bdb1be
hedgehogus <research162@gmail.com>
2017-10-24 02:54:44 +0300

    two way binding

--- TourOfHeroes/src/app/app.component.ts
+++ TourOfHeroes/src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>{{title}}</h1><h2>{{hero.name}} details</h2>',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details</h2>
+  <div><label> id:</label>{{hero.id}}</div>
+  <div>
+    <label> name:</label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 
--- TourOfHeroes/src/app/app.module.ts
+++ TourOfHeroes/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
   providers: [],
   bootstrap: [AppComponent]
