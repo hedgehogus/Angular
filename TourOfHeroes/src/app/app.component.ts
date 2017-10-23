7bf232a11c6fdc0ec9280d1755b990643f8ce089
hedgehogus <research162@gmail.com>
2017-10-23 23:40:39 +0300

    hero

--- TourOfHeroes/src/app/app.component.ts
+++ TourOfHeroes/src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details</h2>',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour Of Heroes';
+  hero: Hero = {
+    id:1,
+    name: "Winstorm"
+  } ;
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
