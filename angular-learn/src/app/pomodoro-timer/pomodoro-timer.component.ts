ffd27c53c567d217b153b497f1b04f69da2c21b3
hedgehogus <research162@gmail.com>
2017-11-20 03:10:39 +0200

    c

--- angular-learn/src/app/pomodoro-timer/pomodoro-timer.component.ts
+++ angular-learn/src/app/pomodoro-timer/pomodoro-timer.component.ts
@@ -6,9 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pomodoro-timer.component.css']
 })
 export class PomodoroTimerComponent implements OnInit {
+    minutes: number;
+    seconds: number;
+    isPaused: boolean;
+    buttonLabel: string;
 
-  constructor() { }
+    constructor() {
+        this.resetPomodoro();
+        // interval timer
+        setInterval(()=> this.tick(), 1000);
+    }
 
+    resetPomodoro(): void {
+        this.minutes = 25;
+        this.seconds = 0;
+        this.buttonLabel = 'start';
+        this.togglePause();
+    }
+
+    private tick():void {
+        if (!this.isPaused){
+            this.buttonLabel = 'Pause';
+            if (--this.seconds < 0) {
+                this.seconds = 59;
+                if (-- this.minutes < 0){
+                    this.resetPomodoro();
+                }
+            }
+        }
+    }
+
+    togglePause(): void {
+        this.isPaused = !this.isPaused;
+        if (this.minutes < 25 /*|| this.seconds > 0*/) {
+            this.buttonLabel = this.isPaused ? 'Resume': 'Pause';
+        }
+    }
+      
   ngOnInit() {
   }
 
