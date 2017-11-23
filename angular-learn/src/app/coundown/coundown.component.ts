0c8cd6b962f6c893482a904f683f6e4a8e6b19d4
hedgehogus <research162@gmail.com>
2017-11-23 22:22:43 +0200

    c

--- angular-learn/src/app/coundown/coundown.component.ts
+++ angular-learn/src/app/coundown/coundown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-coundown',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coundown.component.css']
 })
 export class CoundownComponent implements OnInit {
-  seconds: number = 25;
+  @Input() seconds: number;
   intervalId: any;
 
   constructor() { 
