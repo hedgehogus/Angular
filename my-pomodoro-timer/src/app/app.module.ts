d77cd5ab20c3f42e19fb62ff65a2e83b6e8e3e16
hedgehogus <research162@gmail.com>
2017-12-05 04:40:33 +0200

    pomodoro-tasks component

--- my-pomodoro-timer/src/app/app.module.ts
+++ my-pomodoro-timer/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PomodoroTasksComponent } from './pomodoro-tasks/pomodoro-tasks.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PomodoroTasksComponent
   ],
   imports: [
     BrowserModule
