f8eec454b55507db2b4b751771b89c68a6798e6b
hedgehogus <research162@gmail.com>
2017-12-05 04:38:41 +0200

    i18n pipes

--- angular-learn/src/app/pipes/pipes.component.ts
+++ angular-learn/src/app/pipes/pipes.component.ts
@@ -12,7 +12,20 @@ export class PipesComponent implements OnInit {
   someObj: Object = {
     name:'Eve',
     age: 43
-  } 
+  } ;
+  pomodoros: number = 0;
+  pomodorosWarningMapping: any = {
+    '=0': 'No pomodoros for today',
+    '=1': 'One pomodoro pending',
+    'other': '# pomodoros pending'
+  } ;
+  languageCode: string = 'fr';
+  localizedLabelsMap: any = {
+    'en': 'Start timer',
+    'es': 'Comenzar temporizador',
+    'fr': 'Demarrer une sequence',
+    'other': 'Start timer'
+}
 
   constructor() { }
 
