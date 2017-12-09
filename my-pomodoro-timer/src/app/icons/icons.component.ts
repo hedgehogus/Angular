a492a81b4a644adda2fa8ecf085bf81742dd6a4b
hedgehogus <research162@gmail.com>
2017-12-10 00:16:55 +0200

    c

--- my-pomodoro-timer/src/app/icons/icons.component.ts
+++ my-pomodoro-timer/src/app/icons/icons.component.ts
@@ -16,7 +16,7 @@ export class IconsComponent implements OnInit {
   ngOnInit() {
     this.icons.length = this.task.pomodorosRequired;
     this.icons.fill({name:this.task.name});
-    console.log(this.size);
+   // console.log(this.size);
   }
 
 }
