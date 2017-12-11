bc5aa782e2053311701b349ed3536af978412edc
hedgehogus <research162@gmail.com>
2017-12-11 19:33:30 +0200

    formatted time pipe

---
+++ my-pomodoro-timer/src/app/formattetTimePipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name:'pomodoroFormattedTime',
+    pure:false,
+})
+
+class FormattedTimePipe implements PipeTransform {
+    transform(totalMinutes:number): String {
+        let minutes: number = totalMinutes%60;
+        let hours: number = Math.floor(totalMinutes / 60);
+        return `${hours}h:${minutes}m`;
+    }
+}
