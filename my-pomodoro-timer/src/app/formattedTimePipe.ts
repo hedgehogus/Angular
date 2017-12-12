c39e808006a5404f682d9e48c5a0feb28b158fd9
hedgehogus <research162@gmail.com>
2017-12-12 23:34:17 +0200

    filter pipe

--- my-pomodoro-timer/src/app/formattedTimePipe.ts
+++ my-pomodoro-timer/src/app/formattedTimePipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Task } from './pomodoro-tasks/pomodoro-tasks.component';
 
 @Pipe({
     name:'pomodoroFormattedTime'   
@@ -9,4 +10,16 @@ export class FormattedTimePipe implements PipeTransform {
         let hours: number = Math.floor(totalMinutes / 60);
         return `${hours}h:${minutes}m`;
     }
+}
+
+@Pipe({
+    name: 'pomodoroQueuedOnly',
+    pure: false
+})
+class QueuedOnlyPipe implements PipeTransform {
+    transform(tasks: Task[], ...args:any[]): Task[]{
+    return tasks.filter ((task: Task) => {
+        return task.queued === args[0];
+    });
+}
 }
