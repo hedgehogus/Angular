7a75288722904a8c05791d7a6a05bef6454aa51e
hedgehogus <research162@gmail.com>
2018-01-31 20:57:01 +0200

    timer

--- pomodoro-timer/src/app/app.module.ts
+++ pomodoro-timer/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TimerWidgetComponent } from './timer/timer-widget.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerWidgetComponent
   ],
   imports: [
     BrowserModule
---
+++ pomodoro-timer/src/app/timer/timer-widget.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../shared/shared';
+
+@Component({
+  selector: 'pomodoro-timer-widget',
+  templateUrl: './timer-widget.component.html',
+  styleUrls: ['./timer-widget.component.css']
+})
+export class TimerWidgetComponent implements OnInit {
+  minutes: number;
+  seconds: number;
+  isPaused: boolean;
+  buttonLabelKey: string;
+  buttonLabelsMap: any;
+
+  constructor(private settingsService: SettingsService) { 
+    this.buttonLabelsMap = settingsService.labelsMap.timer;
+  }
+
+  ngOnInit(): void {
+    this.resetPomodoro();
+    setInterval(()=>this.tick(), 1000);
+  }
+
+  resetPomodoro(): void {
+    this.isPaused = true;
+    this.minutes = this.settingsService.timerMinutes - 1;
+    this.seconds = 59;
+    this.buttonLabelKey = 'start';
+  }
+
+  private tick(): void {
+    if (!this.isPaused){
+      this.buttonLabelKey = 'pause';
+
+      if(-- this.seconds < 0) {
+        this.seconds = 59;
+        if(--this.minutes < 0) {
+          this.resetPomodoro();
+        }
+      }
+    }
+  }
+
+  togglePause(): void {
+    this.isPaused = !this.isPaused;
+    if (this.minutes < this.settingsService.timerMinutes || this.seconds < 59) {
+      this.buttonLabelKey = this.isPaused ? 'resume' : 'pause';
+    }
+  }
+
+}
