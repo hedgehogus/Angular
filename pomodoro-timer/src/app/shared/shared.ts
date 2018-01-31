14e85d57b6d2bec66bfed6fbb875c767fcf48869
hedgehogus <research162@gmail.com>
2018-01-31 20:04:59 +0200

    settings service

--- pomodoro-timer/src/app/shared/shared.ts
+++ pomodoro-timer/src/app/shared/shared.ts
@@ -1,11 +1,19 @@
 import Queueable from './interfaces/queuable';
 import Task from './interfaces/task';
+
 import FormattedTimePipe from './pipes/formatted-time.pipe';
-import QueuedOnlyPipe from './pipes/queued-only.pipe'
+import QueuedOnlyPipe from './pipes/queued-only.pipe';
+
+import TaskService from "./services/task.service";
+import SetingsService from "./services/settings.service";
 
 export {
     Queueable,
     Task,
+
     FormattedTimePipe,
-    QueuedOnlyPipe
+    QueuedOnlyPipe,
+    
+    TaskService,
+    SetingsService
 };
